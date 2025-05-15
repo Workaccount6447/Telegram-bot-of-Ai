fc884caf807b07c1e07f5b3a5c78c00ab120d505
Workaccount6447 <sndj86779@gmail.com>
2025-05-15 21:21:26 +0530

    Create bot.py

---
+++ api/bot.py
@@ -0,0 +1,14 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+from api import app
+from aiogram import types, Dispatcher, Bot
+
+import bot.handlers
+from bot import WEBHOOK_PATH, dp, client
+
+@app.post(WEBHOOK_PATH)
+async def bot_webhooks_endpoint(update: dict):
+    telegram_update = types.Update(**update)
+    Dispatcher.set_current(dp)
+    Bot.set_current(client)
+    await dp.process_update(telegram_update)
