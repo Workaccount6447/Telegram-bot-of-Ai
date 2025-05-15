1dcff238e9cf21c48e483aaecefbf349c1c7168f
Workaccount6447 <sndj86779@gmail.com>
2025-05-15 21:25:11 +0530

    Add files via upload

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,4 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+from .start import *
+from .inline import *
---
+++ bot/handlers/inline.py
@@ -0,0 +1,5 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+from aiogram import types
+
+from bot import dp
