b3476e0e7beba6f6ce34e00cb847b3361aeb3a3f
Workaccount6447 <sndj86779@gmail.com>
2025-05-26 20:46:49 +0530

    Delete bot directory

--- bot/__main__.py
+++
@@ -1,7 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-import bot
-
-if __name__ == "__main__":
-    import bot.handlers
-    bot.executor.start_polling(bot.dp, skip_updates=True)
--- bot/handlers/inline.py
+++
@@ -1,5 +0,0 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-from aiogram import types
-
-from bot import dp
