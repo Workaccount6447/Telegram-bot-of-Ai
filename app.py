5e3d312e848a55db4d82ae328ce0fa14f42e7c8c
Workaccount6447 <sndj86779@gmail.com>
2025-08-24 16:55:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,16 +14,18 @@ KVDB_BASE = f"https://kvdb.io/{KVDB_BUCKET}"
 COOKIES = {
     "session-id": "258-2488057-5180660",
     "i18n-prefs": "INR",
-    "lc-acbin": "en_IN",
     "ubid-acbin": "261-4559508-6367401",
-    "at-acbin": "Atza|IwEBIEbJvF6zMWwxVJcwM1cBBLghJa0IfhJpu6guwxQJVjiDQa_k6d-nTd1ewlINt1H717wsqcqhDsLfxZxMcOM26emvQ7eyM0EL0xjPfmPE6S4M3AImz7lEPXzp9OLOEhsASbBF9tLwmBsh9BZN1iW6eS8Mcg3BOAPFJ_hgIReqCW2w76NcI-YHZMF1iCRUV93S1zYKNPEV7uOWSKm6lFA-pwCpXSq7eyuvVIGXV8OydRSO3if46hr0RbZ0OCVZIQd5yUYnO1BR3wcoKiadvX0Te_eJ9l2dYcvEH1f6eShaietZUQ",
-    "sess-at-acbin": "h+U5YvpWslcHY6OcLVwtxT4rDLhKz7Xk98Y4GvGZtXc=",
-    "sst-acbin": "Sst1|PQFv1S46auCLoPSSrLhkPOtTCdR9ElFfR_jOzLlcc1qPpo7d4skHpI37UUs9hYJdUYvVhP2x8cSCBQ_JNnsSDobBdHXdErkDcdETcuEVbPdsa8AGWZopvv4KteadwAJBsRJ8TmcX6xQYaWROTsYv6VXUi3dAE_0nlVP1wMgWJrU8sX-8N98dee741VPb33OryrsdjSlUPOAzChZUblgGlZigMCjv-d8P3uDp1mmSBYpkQiRlLfCFEYJQVVIka6jrAQhjpgm1mGecd30s01IgKVUWl8BH94rcgS6sEJipsOQ82KY",
+    "lc-acbin": "en_IN",
+    "session-token": "kMYueRXHPOnLSoZbPU8gQGM+ZvH4o3kaUmmvAb6O3Sn9n7kf+Mbp15K7ME9/JeNyElnN15loNHbgrhFim2DX+TnAdBioWkCg95K3A2Z3z18WR2YWw/khe4YHPzh7xq49bxKnEpinxvJtomFlcmXK6w8luI9GJrANgdE+Jfz6fAXEHB9OaFCVZBREfZ4SmiqIFlu0euN8vJiiQvyN7MokLZtMCHgm0jgT/1bKvAg4T+dvC1UWdDgXnyyn3WDrWpJrNEMhw7VkWtQdtyBtqwZm8RW4zeBYXT0ACgQX3w2Gt+TDLfi1qMfzGrc7BrnoTog/IJP0aFl1UNm6r2Sv4/ezfuWuBKYmO0BIzgerV/iXtM/aNtDXKbzWvw==",
+    "x-acbin": "DxeegW?GpvOj5BdJQr8fro?VHaJn0x1s38BwqvcdjnOeWcY1fcM1DR@YSTGDqeIv",
+    "at-acbin": "Atza|IwEBIGrPdsKViIFV3vXk45oZyvSTJgXZwGgTDik1420XqGKf19SUvokB5kD8ZUQJJ3fPeRV2zBgIXbRsZ3mlPYcVEvqZRslymsXtIwr4aER9vxSA4C6ynoJoaOPvsSCefVslWCDjcYXOchzH_j72nupTxiyLyVE8Vuoc49VoXJzHzh0kyK8YPoh7MvqhaKEqANuuowpJpprxJ7X3CbDPlUSe0GK22mczsnJKotQgB886JcJ9bA",
+    "sess-at-acbin": "J1itGQHJEtoSeLrRhPBFaCgs/eaBJjSFj+2ICVOImz4=",
+    "sst-acbin": "Sst1|PQE0lr1Nm8snEKqclCaPO0Z0Ce7N1g8X2W0nk0KZ_6IRHbNnYFiN3dfKeZqfjtP47Fe1XOo_yUL3_lfvqgtmqXabSsO5jAzFQ5hHrjkk_KyFkXc2EceMqVpJcdgV209zWJYwoQ4P0p79fSdCg87ogsgDRsA2esUlWDI2ABdvZzZTincS5VRlusQXWfrsqubSloXR3iHzxhmYBu6QPHtWdQVGFmkecFZOT9kDvBY98ntTZMS04Oo1UluLNcQmxNfYSRn01OvUyG5-_Nj-e-XnB9WI-Zr8EuZxoKujQt8UPseGOo8",
     "session-id-time": "2082787201l",
-    "session-token": "92JP3YPrXa2aM7UC3MVeNEU2hwg0UUC0SMwey9/Rfj6zndzFtanm37h2rHWcB17jbLwYIdikWnh7ROHLvi2BZ0kUXu1Z2OA3weWO5yRkKCPvuDyWisI0QGuUxw8deMQGaz4kEI8/0u+GGTx30WeLY7kdIpMpCZxcaOSOnK2z5JEs9ILcgb+XywdyYHrJje0NpqXR9ux1IyOiULtA8UdFmT7LIDcnBYXiK1DLcK49CjmJqK8gGTRDgwdt96qBiNoObLj06ELkTCt66B83rTEy2MrlXsgpv4/DPthRMojSPfeHrrchldTliToqvp2ui3lF3IZucSVYZTuf2fvjIxBOqHCuAqAv9lD1xvNyPA3llI8rte8iAry4oGnNHqtVDytF",
-    "x-acbin": "jNnyveSVr43Beaxh@GIWqPT@DuKtCJbTBAn?tLN0k7Rj3v5YWtB1U@?XUro8Xzsv",
-    "rxc": "ABPYjmcKVz3v13CWRkY"
+    "rxc": "ALP/Gyk0bPxApKunGK4",
+    "csm-hit": "tb:JXRQ8HRKDHCNJ9HGGRD0+s-JXRQ8HRKDHCNJ9HGGRD0|1755534447174&t:1755534447174&adb:adblk_no"
 }
+    
 
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Mobile Safari/537.36"
